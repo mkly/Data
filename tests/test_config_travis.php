5041c41765cca5fbfd579ebc270445d8d702d3eb
Mike Lay <mike@mkly.io>
2014-01-25 20:44:28 -0800

    Add Travis CI

---
+++ tests/test_config_travis.php
@@ -0,0 +1,9 @@
+<?php
+$config = array(
+	'db-server' => 'localhost',
+	'db-username' => 'travis',
+	'db-database' => 'test',
+	'admin-password' => 'admin',
+	'target' => dirname(__FILE__) . '/core/concrete5/web',
+	'starting-point' => 'data_test'
+);
