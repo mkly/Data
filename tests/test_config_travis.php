0b1e0e6f008666350c29a89a4447125731182e80
Mike Lay <mike@mkly.io>
2014-01-26 18:29:30 -0800

    Fix travis for db tests

--- tests/test_config_travis.php
+++ tests/test_config_travis.php
@@ -3,6 +3,7 @@ $config = array(
 	'db-server' => 'localhost',
 	'db-username' => 'travis',
 	'db-database' => 'test',
+	'db-password' => '',
 	'admin-password' => 'admin',
 	'target' => dirname(__FILE__) . '/core/concrete5/web',
 	'starting-point' => 'data_test'
