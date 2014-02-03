9d10dad6c081d75a1a9a5ac448876e98cdc4be8e
Mike Lay <mike@mkly.io>
2014-02-03 15:46:58 -0800

    Format Data Type list

--- src/data/single_pages/dashboard/data/management/view.php
+++ src/data/single_pages/dashboard/data/management/view.php
@@ -1,8 +1,10 @@
 <?php defined('C5_EXECUTE') or die('Access Denied.') ?>
 <?= $dashboard->getDashboardPaneHeaderWrapper(t('Data Types'), false, false) ?>
-<ul>
-<?php foreach ($dataTypes as $dataType) { ?>
-	<li><a href="<?= $this->url('/dashboard/data/management/search', $dataType->dtID) ?>"><?= $dataType->dtName ?></a></li>
-<?php } ?>
-</ul>
+<table class="ccm-results-list">
+	<?php $alt = ' ccm-list-record-alt'; foreach ($dataTypes as $dataType) { ?>
+		<tr class="ccm-list-record<?= $alt ?>">
+			<td><a href="<?= $this->url('/dashboard/data/management/search', $dataType->dtID) ?>"><?= $dataType->dtName ?></a></td>
+		</tr>
+	<?php $alt = $alt ? '' : ' ccm-list-record-alt'; } ?>
+</table>
 <?= $dashboard->getDashboardPaneFooterWrapper(true) ?>
