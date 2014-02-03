1529cb08ed038c1c10e369fc757ac8f97ab26145
Mike Lay <mike@mkly.io>
2014-02-03 15:48:30 -0800

    Hide list when no datas

--- src/data/single_pages/dashboard/data/management/search.php
+++ src/data/single_pages/dashboard/data/management/search.php
@@ -11,17 +11,19 @@
 <div class="ccm-pane-body">
 	<?= $interface->button(t('Create'), $this->url('/dashboard/data/management/create', $dataType->dtID)) ?>
 	<div class="clearfix"></div>
-	<table class="ccm-results-list">
-		<tr class="ccm-results-list-header">
-			<th><?= t('Name') ?></th>
-		</tr>
-	<?php $alt = ''; foreach ($datas as $data) { ?>
-		<tr class="ccm-list-record<?= $alt ?>">
-			<td><?= $data->name ? $data->name->getValue('display_sanitized') : $data->dID ?><?= $interface->button(t('Edit'), $this->url('/dashboard/data/management/edit', $dataType->dtID, $data->dID)) ?></td>
-		</tr>
-	<?php $alt = $alt ? '' : ' ccm-list-record-alt'; } ?>
-	</table>
-	<?php $DataList->displayPagingV2() ?>
+	<?php if ($datas) { ?>
+		<table class="ccm-results-list">
+			<tr class="ccm-results-list-header">
+				<th><?= t('Name') ?></th>
+			</tr>
+		<?php $alt = ''; foreach ($datas as $data) { ?>
+			<tr class="ccm-list-record<?= $alt ?>">
+				<td><?= $data->name ? $data->name->getValue('display_sanitized') : $data->dID ?><?= $interface->button(t('Edit'), $this->url('/dashboard/data/management/edit', $dataType->dtID, $data->dID)) ?></td>
+			</tr>
+		<?php $alt = $alt ? '' : ' ccm-list-record-alt'; } ?>
+		</table>
+		<?php $DataList->displayPagingV2() ?>
+	<?php } ?>
 </div>
 <div class="ccm-pane-footer">
 </div>
