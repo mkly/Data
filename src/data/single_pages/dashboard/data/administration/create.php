d2d412b4e6fd3a75cf2ccf9880ca3650df5b6d85
Mike Lay <mike@mkly.io>
2014-02-06 16:20:36 -0800

    Switch these before I get to used to it

--- src/data/single_pages/dashboard/data/administration/create.php
+++ src/data/single_pages/dashboard/data/administration/create.php
@@ -2,10 +2,10 @@
 <?= $dashboard->getDashboardPaneHeaderWrapper(t('Create Data Type'), false, false, false) ?>
 <form method="post" action="<?= $this->action('create') ?>">
 	<div class="ccm-pane-body">
-		<?= $form->label('dtName', t('Name')) ?>
-		<?= $form->text('dtName') ?>
 		<?= $form->label('dtHandle', t('Handle')) ?>
 		<?= $form->text('dtHandle') ?>
+		<?= $form->label('dtName', t('Name')) ?>
+		<?= $form->text('dtName') ?>
 	</div>
 	<div class="ccm-pane-footer">
 		<div class="ccm-buttons">
