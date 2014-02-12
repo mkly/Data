5ec3e8f00776de2a6e79e60a84a9d74447f54c57
Mike Lay <mike@mkly.io>
2014-02-12 12:25:10 -0800

    Stub out Belongs To

--- src/data/models/attribute/types/data_belongs_to/controller.php
+++ src/data/models/attribute/types/data_belongs_to/controller.php
@@ -1,16 +1,4 @@
 <?php
 defined('C5_EXECUTE') or die('Access Denied.');
 
-class DataBelongsToAttributeTypeController extends AttributeTypeController {
-
-	public function type_form() {
-		echo Loader::helper('form/data_type_selector', 'data')->selectDataType('dtID');
-	}
-
-	/**
-	 * @param array $data
-	 */
-	public function saveKey($data) {
-	}
-
-}
+class DataBelongsToAttributeTypeController extends AttributeTypeController {}
