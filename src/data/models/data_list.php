703de2c0cb2faca2ca72c7179ce7aaea851bd29f
Remo Laubacher <remo.laubacher@gmail.com>
2014-03-12 08:43:19 +0100

    first commit to get iterator functionality into "Data"

--- src/data/models/data_list.php
+++ src/data/models/data_list.php
@@ -1,12 +1,15 @@
 <?php
 defined('C5_EXECUTE') or die('Access Denied.');
 
-class DataList extends DatabaseItemList {
+class DataList extends DatabaseItemList implements Iterator {
 
 	protected $dataType;
 	protected $attributeClass = 'DataAttributeKey';
 	protected $attributeFilters = array();
-
+	protected $cursor = false;
+	protected $row = false;
+	protected $eof = false;
+	
 	/**
 	 * @param $dataType DataType
 	 */
@@ -83,4 +86,85 @@ class DataList extends DatabaseItemList {
 		}
 		$this->filterByAttribute($handle, $args[0]);
 	}
+
+	/** 
+	 * Returns an array of whatever objects extends this class (e.g. PageList returns a list of pages).
+	 */
+	public function getRow($itemsToGet = 0, $offset = 0) {
+		$q = $this->executeBase();
+		
+		// handle order by
+		$this->setupAttributeSort();
+		$this->setupAutoSort();
+		$this->setupSortByString();
+		
+		if ($this->sortByString != '') {
+			$q .= 'order by ' . $this->sortByString . ' ';
+		}	
+		if ($this->itemsPerPage > 0 && (intval($itemsToGet) || intval($offset)) ) {
+			$q .= 'limit ' . $offset . ',' . $itemsToGet . ' ';
+		}
+		
+		$db = Loader::db();
+		if ($this->debug) { 
+			Database::setDebug(true);
+		}
+		$resp = $db->Execute($q);
+		if ($this->debug) { 
+			Database::setDebug(false);
+		}
+		
+		$this->start = $offset;
+		return $resp;
+	}
+	
+	/**
+	 * Returns the numeric key of the current data element
+	 * @return int
+	 */
+	public function key() {
+		return $this->row['dID'];
+	}
+	
+	/**
+	 * Returns the current Data element
+	 * @return \Data
+	 */
+	public function current() {
+		$data = new Data;
+		$data->Load('dID=?', array($this->row['dID']));
+		return $data;
+	}
+	
+	/**
+	 * Move forward to next element in iterator
+	 */
+	public function next() {
+		// initialize cursor
+		if (!$this->cursor) {
+			$this->rewind();
+		}
+
+		$this->ind++;        
+		$this->row = $this->cursor->FetchRow();
+		$this->eof = !is_array($this->row);        
+	}
+	
+	/**
+	 * Rewind iterator to start with first element
+	 */
+	public function rewind() {
+		if ($this->getQuery() == '') {
+			$this->setBaseQuery();
+		}
+		$this->cursor = $this->getRow($itemsToGet, $offset);
+	}
+
+	/**
+	 * Returns true if we're at a valid position
+	 * @return bool
+	 */
+	public function valid() {
+		return !$this->eof;
+	}	
 }
