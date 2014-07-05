62f81717ee41760406bc4023a4c498f3e6741b43
Remo Laubacher <remo.laubacher@gmail.com>
2014-07-05 19:37:10 +0200

    silly me, test is already taken, let's try testS

--- tests/test_config_travis.php
+++ tests/test_config_travis.php
@@ -2,7 +2,7 @@
 $config = array(
 	'db-server' => '127.0.0.1',
 	'db-username' => 'travis',
-	'db-database' => 'test',
+	'db-database' => 'tests',
 	'db-password' => '',
 	'admin-password' => 'admin',
 	'target' => dirname(__FILE__) . '/core/concrete5/web',
